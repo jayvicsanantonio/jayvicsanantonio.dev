ec0de9cc28627da14acfd800ae39e7d0347d5839
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2023-02-19 19:28:31 -0800

    Add initial content and styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,74 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="outer-page">
+      <span className="tags top-tags">
+        &lt;html&gt;
+        <br/>
+      </span>
+      <section className="inner-page">
+        <div className="home-section">
+          <h1 aria-label="Hi, I'm Jayvic, full stack web developer">
+            <span className="wiggle" aria-hidden="true">H</span>
+            <span className="wiggle" aria-hidden="true">i</span>
+            <span className="wiggle" aria-hidden="true">,</span>
+            <br/>
+            <span className="wiggle" aria-hidden="true">I</span>
+            <span className="wiggle" aria-hidden="true">'</span>
+            <span className="wiggle" aria-hidden="true">m</span>
+            <span> </span>
+            <span className="wiggle" aria-hidden="true">J</span>
+            <span className="wiggle" aria-hidden="true">a</span>
+            <span className="wiggle" aria-hidden="true">y</span>
+            <span className="wiggle" aria-hidden="true">v</span>
+            <span className="wiggle" aria-hidden="true">i</span>
+            <span className="wiggle" aria-hidden="true">c</span>
+            <span className="wiggle" aria-hidden="true">,</span>
+            <br />
+            <span className="wiggle" aria-hidden="true">F</span>
+            <span className="wiggle" aria-hidden="true">u</span>
+            <span className="wiggle" aria-hidden="true">l</span>
+            <span className="wiggle" aria-hidden="true">l</span>
+            <span> </span>
+            <span className="wiggle" aria-hidden="true">S</span>
+            <span className="wiggle" aria-hidden="true">t</span>
+            <span className="wiggle" aria-hidden="true">a</span>
+            <span className="wiggle" aria-hidden="true">c</span>
+            <span className="wiggle" aria-hidden="true">k</span>
+            <span> </span>
+            <span className="wiggle" aria-hidden="true">W</span>
+            <span className="wiggle" aria-hidden="true">e</span>
+            <span className="wiggle" aria-hidden="true">b</span>
+            <span> </span>
+            <span className="wiggle" aria-hidden="true">D</span>
+            <span className="wiggle" aria-hidden="true">e</span>
+            <span className="wiggle" aria-hidden="true">v</span>
+            <span className="wiggle" aria-hidden="true">e</span>
+            <span className="wiggle" aria-hidden="true">l</span>
+            <span className="wiggle" aria-hidden="true">o</span>
+            <span className="wiggle" aria-hidden="true">p</span>
+            <span className="wiggle" aria-hidden="true">e</span>
+            <span className="wiggle" aria-hidden="true">r</span>
+          </h1>
+          <p>
+            <span>React.js</span>
+            <span>Ember.js</span>
+            <span>Node.js</span>
+            <span>Express.js</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>AWS</span>
+            <span>Java</span>
+            <span>MongoDB</span>
+            <span>Redis</span>
+            <span>MySQL</span>
+            <span>PostgreSQL</span>
+          </p>
+        </div>
+      </section>
+      <span className="tags bottom-tags">
+        <br/>
+        &lt;/html&gt;
+      </span>
     </div>
   );
 }
