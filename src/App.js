114736a7bc2639d9a9aa74371b04c46084045484
Jayvic San Antonio <sanantonio.jayvic@gmail.com>
2023-03-06 22:10:06 -0800

    Added content with js, css and html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer-page">
+      <span className="tags top-tags">
+        &lt;html&gt;
+        <br />
+      </span>
+      <section className="inner-page">
+        <div className="home-section">
+          <h1 aria-label="Hi, I'm Jayvic, full stack web developer">
+            <span className="wiggle" aria-hidden="true">
+              H
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              i
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              ,
+            </span>
+            <br />
+            <span className="wiggle" aria-hidden="true">
+              I
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              '
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              m
+            </span>
+            <span className="space"> </span>
+            <span className="wiggle" aria-hidden="true">
+              J
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              a
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              y
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              v
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              i
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              c
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              ,
+            </span>
+            <br />
+            <span className="wiggle" aria-hidden="true">
+              F
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              u
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              l
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              l
+            </span>
+            <span className="space"> </span>
+            <span className="wiggle" aria-hidden="true">
+              S
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              t
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              a
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              c
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              k
+            </span>
+            <span className="space"> </span>
+            <span className="wiggle" aria-hidden="true">
+              W
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              e
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              b
+            </span>
+            <span className="space"> </span>
+            <span className="wiggle" aria-hidden="true">
+              D
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              e
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              v
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              e
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              l
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              o
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              p
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              e
+            </span>
+            <span className="wiggle" aria-hidden="true">
+              r
+            </span>
+          </h1>
+          <p>
+            <span>React.js</span>
+            <span>Ember.js</span>
+            <span>Node.js</span>
+            <span>Express.js</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>AWS</span>
+            <span>Java</span>
+            <span>MongoDB</span>
+            <span>Redis</span>
+            <span>MySQL</span>
+          </p>
+        </div>
+      </section>
+      <span className="tags bottom-tags">
+        <br />
+        &lt;/html&gt;
+      </span>
     </div>
   );
 }
