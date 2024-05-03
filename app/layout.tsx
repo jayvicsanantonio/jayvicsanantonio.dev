390e18e7e52c80e5972cade9d8a5d8e7ae6a4fd5
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-05-03 12:58:17 -0700

    Add Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${inter.variable} max-w-5xl px-12 flex flex-col md:flex-row mx-4 lg:mx-auto min-h-screen bg-gray-950 text-gray-50`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
