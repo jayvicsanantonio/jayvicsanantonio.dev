524f7e7e0535be7c20817d146c7f91bb663e3c72
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-05-09 17:44:20 -0700

    Add noopener and noreferrer for external links to ensure added security

--- components/pages/home/ViewProjectButton.tsx
+++ components/pages/home/ViewProjectButton.tsx
@@ -6,6 +6,7 @@ export default function ViewProjectButton({ link }: { link: string }) {
       className="transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-flex items-center justify-center rounded-md bg-gray-800 px-6 py-3 text-sm font-medium text-gray-200  focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 dark:bg-gray-200 dark:text-gray-900 dark:hover:bg-gray-300 dark:focus:ring-gray-600 dark:focus:ring-offset-gray-950"
       href={link}
       target="_blank"
+      rel="noopener noreferrer"
     >
       View Project
     </Link>
