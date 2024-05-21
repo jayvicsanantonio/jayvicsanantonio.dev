ef1844029927b9d39a1cf909ac4ba6eaf22f86dd
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-05-21 16:43:51 -0700

    Fix Latest Blog Post Breadcrumb

--- app/blog/post-layoff-adventure/page.tsx
+++ app/blog/post-layoff-adventure/page.tsx
@@ -6,7 +6,7 @@ import CalendarIcon from "@/components/icons/calendar";
 export default function Page() {
   return (
     <article className="prose prose-invert mx-auto space-y-8">
-      <BlogBreadcrumb title="The Typescript Tightrope" />
+      <BlogBreadcrumb title='From Yahoo Y! to "Yahoo, New Opportunities!"' />
       <div className="space-y-2 not-prose">
         <h1 className="font-oswald text-gray-200 leading-tight text-3xl md:text-4xl font-title font-bold tracking-tight">
           From Yahoo Y! to "Yahoo, New Opportunities!": My Post-Layoff Adventure
