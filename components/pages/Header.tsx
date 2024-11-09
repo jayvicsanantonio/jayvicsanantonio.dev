b707cfa405169f77b62139833610ec42a11f41b8
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-11-09 14:45:58 -0800

    Fix error

--- components/pages/Header.tsx
+++ components/pages/Header.tsx
@@ -4,13 +4,10 @@ import { useState, useCallback } from "react";
 import Link from "next/link";
 import Drawer from "./Drawer";
 import MainMenu from "./MainMenu";
-import useBoop from "@/hooks/use-boop";
-import { animated } from "react-spring";
 import { Button } from "@/components/ui/button";
 import { Code, Menu } from "lucide-react";
 
 export default function Header() {
-  const [style, trigger] = useBoop({ rotation: 20, timing: 200, scale: 1.4 });
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const closeDrawer = useCallback(() => {
@@ -19,15 +16,11 @@ export default function Header() {
 
   return (
     <header className="flex items-center justify-between px-4 py-6 bg-gray-950 border-b border-gray-800 sticky top-0 z-10 -mx-4 lg:-mx-auto">
-      <Link
-        href="/"
-        className="flex items-center gap-2"
-        aria-label="Logo"
-        onMouseEnter={trigger}
-      >
-        <animated.span style={style}>
-          <Code size={24} className="hover:text-violet-600" />
-        </animated.span>
+      <Link href="/" className="flex items-center gap-2" aria-label="Logo">
+        <Code
+          size={24}
+          className="hover:text-violet-600 hover:scale-110 transform"
+        />
       </Link>
       <nav
         role="navigation"
