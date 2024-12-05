3ca56d21c0804cb55c6cb99f409943b37606c610
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-12-05 07:37:32 -0800

    Attach Holiday Lights to Header

--- components/pages/home/Hero.tsx
+++ components/pages/home/Hero.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import Bluesky from "@/components/icons/Bluesky";
-import HolidayLights from "@/components/pages/HolidayLights";
 import ScrollDown from "@/components/pages/ScrollDown";
 import SocialMediaIconButton from "@/components/pages/home/SocialMediaIconButton";
 import { motion } from "framer-motion";
@@ -34,71 +33,68 @@ export default function Hero({
   }, []);
 
   return (
-    <>
-      <HolidayLights />
-      <section className="relative h-screen flex flex-col-reverse justify-end md:flex-row items-center px-4 pb-60 text-gray-950 dark:text-gray-200">
-        <div className="space-y-6">
-          <div className="font-oswald lg:space-y-2 flex flex-col items-center md:items-start">
-            <h1 className="font-title text-4xl font-bold leading-snug lg:text-6xl ">
-              Hey, I'm Jayvic 👋
-            </h1>
-            <motion.h2
-              key={activeIndex}
-              initial={{ y: 10, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: -10, opacity: 0 }}
-              transition={{ duration: 0.5 }}
-              className="text-lg lg:text-3xl font-light uppercase bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500 will-change-transform"
-            >
-              {roles[activeIndex]}
-            </motion.h2>
-          </div>
-          <p className="text-lg lg:text-xl text-center md:text-left">
-            Turning caffeine into code and transforming challenges into
-            innovative web solutions that make a difference.
-          </p>
-          <div className="flex space-x-6 justify-center md:justify-start">
-            <SocialMediaIconButton
-              Icon={Github}
-              link="https://github.com/jayvicsanantonio"
-            >
-              Github
-            </SocialMediaIconButton>
-            <SocialMediaIconButton
-              Icon={Linkedin}
-              link="https://www.linkedin.com/in/jayvicsanantonio/"
-            >
-              LinkedIn
-            </SocialMediaIconButton>
-            <SocialMediaIconButton
-              Icon={Bluesky}
-              link="https://bsky.app/profile/jayvicsanantonio.dev"
-            >
-              Bluesky
-            </SocialMediaIconButton>
-            <SocialMediaIconButton
-              Icon={Mail}
-              link="mailto:hi@jayvicsanantonio.dev"
-            >
-              Email
-            </SocialMediaIconButton>
-          </div>
+    <section className="relative h-screen flex flex-col-reverse justify-end md:flex-row items-center px-4 pb-60 text-gray-950 dark:text-gray-200">
+      <div className="space-y-6">
+        <div className="font-oswald lg:space-y-2 flex flex-col items-center md:items-start">
+          <h1 className="font-title text-4xl font-bold leading-snug lg:text-6xl ">
+            Hey, I'm Jayvic 👋
+          </h1>
+          <motion.h2
+            key={activeIndex}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-lg lg:text-3xl font-light uppercase bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-500 will-change-transform"
+          >
+            {roles[activeIndex]}
+          </motion.h2>
         </div>
-        <Image
-          alt="Profile"
-          className="rounded-full md:mb-0 mb-8 p-1.5"
-          height={340}
-          loading="eager"
-          src="/images/home/profile-image.jpeg"
-          style={{
-            aspectRatio: "340/340",
-            objectFit: "cover",
-          }}
-          width={340}
-          priority={true}
-        />
-        <ScrollDown sectionRef={aboutRef} />
-      </section>
-    </>
+        <p className="text-lg lg:text-xl text-center md:text-left">
+          Turning caffeine into code and transforming challenges into innovative
+          web solutions that make a difference.
+        </p>
+        <div className="flex space-x-6 justify-center md:justify-start">
+          <SocialMediaIconButton
+            Icon={Github}
+            link="https://github.com/jayvicsanantonio"
+          >
+            Github
+          </SocialMediaIconButton>
+          <SocialMediaIconButton
+            Icon={Linkedin}
+            link="https://www.linkedin.com/in/jayvicsanantonio/"
+          >
+            LinkedIn
+          </SocialMediaIconButton>
+          <SocialMediaIconButton
+            Icon={Bluesky}
+            link="https://bsky.app/profile/jayvicsanantonio.dev"
+          >
+            Bluesky
+          </SocialMediaIconButton>
+          <SocialMediaIconButton
+            Icon={Mail}
+            link="mailto:hi@jayvicsanantonio.dev"
+          >
+            Email
+          </SocialMediaIconButton>
+        </div>
+      </div>
+      <Image
+        alt="Profile"
+        className="rounded-full md:mb-0 mb-8 p-1.5"
+        height={340}
+        loading="eager"
+        src="/images/home/profile-image.jpeg"
+        style={{
+          aspectRatio: "340/340",
+          objectFit: "cover",
+        }}
+        width={340}
+        priority={true}
+      />
+      <ScrollDown sectionRef={aboutRef} />
+    </section>
   );
 }
