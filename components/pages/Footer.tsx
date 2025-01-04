ccbcd11b95c9075bcd4426bfc2d4ffedc9c7cfa5
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-01-03 18:17:48 -0800

    Update Copyright Year

--- components/pages/Footer.tsx
+++ components/pages/Footer.tsx
@@ -5,10 +5,12 @@ import { Github, Linkedin, Mail } from "lucide-react";
 import SocialMediaIconButton from "@/components/pages/home/SocialMediaIconButton";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-white dark:bg-gray-950 -mx-4 lg:-mx-auto md:px-8 py-6 text-gray-950 dark:text-gray-200 border-t dark:border-gray-800">
       <div className="mx-auto flex flex-col md:flex-row items-center justify-between gap-2">
-        <p>© 2024 Jayvic San Antonio. All rights reserved.</p>
+        <p>© {currentYear} Jayvic San Antonio. All rights reserved.</p>
         <div className="flex items-center gap-6">
           <SocialMediaIconButton
             Icon={Github}
