3db7591d68e471ec8fc7df61820e5355722a6203
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-03-23 23:20:54 -0700

    Setup PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,57 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import nextPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withPWA = nextPWA({
+  dest: "public",
+  register: true, // Register the service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+});
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+const nextConfig = {
+  reactStrictMode: true,
 
-org: "jayvic-san-antonio",
-project: "javascript-nextjs",
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+};
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+export default withPWA(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  org: "jayvic-san-antonio",
+  project: "javascript-nextjs",
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
 });
